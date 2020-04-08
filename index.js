295613e5021b851f1f42cbba1a8d0ab0ef563140
lakshansud <lakshansud@gmail.com>
2020-04-08 19:46:05 +0530

    3 files modified

--- index.js
+++ index.js
@@ -1,68 +1,8 @@
-import React, { Component } from 'react';
-import { render } from 'react-dom';
-import Hello from './Hello';
-import './style.css';
-
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Router, Route, Link, browserHistory, IndexRoute } from 'react-router'
+import App from './Hello';
 
-class App extends React.Component {
-   render() {
-      return (
-         <div>
-            <ul>
-            <li>Home</li>
-            <li>About</li>
-            <li>Contact</li>
-            </ul>
-            {this.props.children}
-         </div>
-      )
-   }
-}
-export default App;
-
-class Home extends React.Component {
-   render() {
-      return (
-         <div>
-            <h1>Home...</h1>
-         </div>
-      )
-   }
-}
-export default Home;
-
-class About extends React.Component {
-   render() {
-      return (
-         <div>
-            <h1>About...</h1>
-         </div>
-      )
-   }
-}
-export default About;
-
-class Contact extends React.Component {
-   render() {
-      return (
-         <div>
-            <h1>Contact...</h1>
-         </div>
-      )
-   }
-}
-export default Contact;
-
-ReactDOM.render((
-   <Router history = {browserHistory}>
-      <Route path = "/" component = {App}>
-         <IndexRoute component = {Home} />
-         <Route path = "home" component = {Home} />
-         <Route path = "about" component = {About} />
-         <Route path = "contact" component = {Contact} />
-      </Route>
-   </Router>
-), document.getElementById('app'))
+ReactDOM.render(
+<App />,
+document.getElementById('root')
+);
