8f24aef5c25b5faa47b8c59b74d324f6a583d61e
Lakshan Danapala <lakshan@alliontechnologies.com>
2020-04-10 12:55:03 +0530

    Add to cart function coded

--- src/foodordering.js
+++ src/foodordering.js
@@ -14,10 +14,10 @@ export default class FoodOrderingForm extends React.Component {
         super(props);
         this.state = {
             value: '', foodList: [
-                { id: 1, name: 'Mutton Pizza', price: 600.00, image: 'https://previews.123rf.com/images/vvmich/vvmich1706/vvmich170600521/81224018-traditional-margarita-pizza.jpg' },
-                { id: 2, name: 'Hot Butter Prawns', price: 650.00, image: 'https://assets.bonappetit.com/photos/58a4e12a9fda6d7fbc740e91/16:9/w_2560,c_limit/shrimp-scampi.jpg' },
-                { id: 3, name: 'Hot Butter Cuttlefish', price: 650.00, image: 'https://media-cdn.tripadvisor.com/media/photo-i/15/09/f1/2a/hot-butter-cuttlefish.jpg' },
-                { id: 4, name: 'Fried Rice With Chicken', price: 500.00, image: 'https://vaya.in/recipes/wp-content/uploads/2018/03/Chicken-Singaporean-Fried-Rice.jpg' }
+                { id: 1, name: 'Mutton Pizza', quantity: 0, price: 600.00, image: 'https://previews.123rf.com/images/vvmich/vvmich1706/vvmich170600521/81224018-traditional-margarita-pizza.jpg' },
+                { id: 2, name: 'Hot Butter Prawns', quantity: 0, price: 650.00, image: 'https://assets.bonappetit.com/photos/58a4e12a9fda6d7fbc740e91/16:9/w_2560,c_limit/shrimp-scampi.jpg' },
+                { id: 3, name: 'Hot Butter Cuttlefish', quantity: 0, price: 650.00, image: 'https://media-cdn.tripadvisor.com/media/photo-i/15/09/f1/2a/hot-butter-cuttlefish.jpg' },
+                { id: 4, name: 'Fried Rice With Chicken', quantity: 0, price: 500.00, image: 'https://vaya.in/recipes/wp-content/uploads/2018/03/Chicken-Singaporean-Fried-Rice.jpg' }
             ] };
     }
 
@@ -29,14 +29,13 @@ export default class FoodOrderingForm extends React.Component {
         return this.state.foodList.map((list) => {
             const { id, name, price, image } = list;
             return (
-              
                 <div className="col-2 text-center">
                     <div key={id}>
                         <img src={image} className="img my-2" />
                     </div>
                     <h5>{name}</h5>
                     <p className="price">RS.{price}</p> 
-                    <OrderingModal orderItem={name}/>
+                    <OrderingModal orderItem={list} />
                 </div>
             );
         });
--- src/orderconfirmmodal.js
+++ src/orderconfirmmodal.js
@@ -25,8 +25,25 @@ const OrderingModal = createReactClass({
         this.setState({ showModal: true });
     },
 
-    render() {
+    confirm() {
+        var data = [];
+        this.props.orderItem.quantity = this.state.quantity;
+        var savedobject = JSON.parse(localStorage.getItem("orderedItems"));
+        if (savedobject) {
+            data = savedobject;
+            data.push(this.props.orderItem);
+        } else {
+            data.push(this.props.orderItem);
+        }
+        localStorage.setItem('orderedItems', JSON.stringify(data));
+        this.setState({ showModal: false });
+    },
 
+    handleChange(event) {
+        this.setState({ quantity: event.target.value });
+    },
+
+    render() {
         return (
             <div>
                 <Button
@@ -36,19 +53,18 @@ const OrderingModal = createReactClass({
                 >
                     Add to cart
         </Button>
-
                 <Modal show={this.state.showModal} onHide={this.close}>
                     <Modal.Header closeButton>
-                        <Modal.Title>{ this.props.orderItem }</Modal.Title>
+                        <Modal.Title>{this.props.orderItem.name}</Modal.Title>
                     </Modal.Header>
                     <Modal.Body>
                         <label>
-                            Quantity : <input type="number" name="quantity" />
+                            Quantity : <input type="number" name="quantity" onChange={this.handleChange} required/>
                         </label>
                      
                     </Modal.Body>
                     <Modal.Footer>
-                        <Button onClick={this.close}>Confim</Button>
+                        <Button onClick={this.confirm}>Confim</Button>
                         <Button onClick={this.close}>Close</Button>
                     </Modal.Footer>
                 </Modal>
